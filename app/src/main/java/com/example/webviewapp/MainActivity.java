cf598f1e52d290cf08ea2b38ef57d9219b639a2e
Simon <a20simst@student.his.se>
2021-04-06 09:55:30 +0200

    Private member variable created and instantiate

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -13,7 +14,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    // private members
+     private WebView myWebView;
     public void showExternalWebPage(){
         // TODO: Add your code for showing external web page here
     }
@@ -28,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        WebView myWebView = findViewById(R.id.my_webview);
         /*
         * Rename your App. Tip: Values->Strings
         * Enable Internet access for your App. Tip: Manifest
